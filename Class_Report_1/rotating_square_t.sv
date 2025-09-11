71037adef1909436cc90f5ce0833bf48823555a9
Nicholas Storti <130611951+1995mazdamiata@users.noreply.github.com>
2025-09-10 23:33:17 -0500

    Update rotating_square_t.sv

--- Class_Report_1/rotating_square_t.sv
+++ Class_Report_1/rotating_square_t.sv
@@ -56,8 +56,8 @@ module rotating_square_t;
         #400 cw = 1;
         #200 en = 0;
         #40  en = 1;
-        #200 cw = 0;
-        #200 $finish;
+        #300 cw = 0;
+        #100 $finish;
     end
     
 endmodule
